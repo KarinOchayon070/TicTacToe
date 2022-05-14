4b0a4b2ba1d986c7a3452d7486db624a4211f35c
KarinOchayon070 <karinoch070@gmail.com>
2022-05-14 13:46:09 +0300

    human VS human is WORKING!!!  :)

--- UI.py
+++ UI.py
@@ -23,13 +23,6 @@ root.iconbitmap('images/icon.ico')
 #---------------------------------------------------------------------------------When "human VS human"---------------------------------------------------------------------------------#
 
 
-# Decleration of the variables
-player1 = StringVar()
-player2 = StringVar()
-name1_var = tk.StringVar()
-name2_var = tk.StringVar()
-
-
 # Func when press
 def human_VS_human():
     # Clear the board
@@ -42,11 +35,9 @@ def human_VS_human():
     create_board()
     btnClick(buttons)
     checkForWin()
-    disableButton()
 
 
 #--------------------------------------------------------------Buttons ("human VC humam", "human VC pc")--------------------------------------------------------------------#
-
 # Import the images (buttons) using PhotoImage function
 click_btn1 = PhotoImage(file="images/humanVShuman.png")
 click_btn2 = PhotoImage(file="images/humanVSpc.png")
@@ -63,23 +54,19 @@ human_VS_pc_but = tk.Button(root, image=click_btn2, bg="white",
                             borderwidth=0)
 human_VS_pc_but.pack(side="left", padx=110, pady=0.01)
 
+
 #---------------------------------------------------------------------------------Function---------------------------------------------------------------------------------#
-
-bclick = True
-flag = 0
-
-
-# main_frame = Frame(root, bg="cadet blue", pady=2,
-#                    width=1800, height=1000, relief=RIDGE)
-# main_frame.pack(fill=BOTH, expand=True)
-
-# left_frame = Frame(main_frame, bg="powder blue", pady=10,
-#                    width=200, height=550, relief=RIDGE)
-# left_frame.pack(side=LEFT)
-
-# right_frame = Frame(main_frame, bg="powder blue", pady=10,
-#                     width=200, height=550, relief=RIDGE)
-# right_frame.pack(side=RIGHT)
+# Decleration of the variables
+player1 = StringVar()
+player2 = StringVar()
+player_1_name = StringVar()
+player_2_name = StringVar()
+player_1_score = IntVar()
+player_2_score = IntVar()
+whos_turn = StringVar()
+playerTurn = StringVar()
+playerTurn.set("X")
+flag = IntVar()
 
 
 # Function to create the board (9 buttons in total)
@@ -127,16 +114,41 @@ def create_board():
     button9.grid(row=2, column=2,  sticky=S+N+E+W)
 
 
-def disableButton():
-    button1.configure(state=DISABLED)
-    button2.configure(state=DISABLED)
-    button3.configure(state=DISABLED)
-    button4.configure(state=DISABLED)
-    button5.configure(state=DISABLED)
-    button6.configure(state=DISABLED)
-    button7.configure(state=DISABLED)
-    button8.configure(state=DISABLED)
-    button9.configure(state=DISABLED)
+def btnClick(buttons):
+
+    whos_turn_label = Label(root, textvariable=whos_turn, width=20, borderwidth=2,
+                            bg='white', font=('COMIC SANS MS', 15))
+    whos_turn_label.grid(row=2, column=7)
+    whos_turn.set(f'{player_1_name.get()} turn')
+
+    if buttons["text"] == " " and playerTurn.get() == "X":
+        buttons["text"] = "X"
+        playerTurn.set("O")
+        checkForWin()
+        flag.set(flag.get() + 1)
+        whos_turn.set(f'{player_2_name.get()} turn')
+
+    elif(buttons["text"] == " " and playerTurn.get() == "O"):
+        buttons["text"] = "O"
+        playerTurn.set("X")
+        checkForWin()
+        flag.set(flag.get() + 1)
+
+    else:
+        messagebox.showinfo(
+            "Tic Tac Toe", "Button already clicked, please try again")
+
+
+def handleButtonState(state):
+    button1.configure(state=state)
+    button2.configure(state=state)
+    button3.configure(state=state)
+    button4.configure(state=state)
+    button5.configure(state=state)
+    button6.configure(state=state)
+    button7.configure(state=state)
+    button8.configure(state=state)
+    button9.configure(state=state)
 
 
 def checkForWin():
@@ -146,52 +158,46 @@ def checkForWin():
         button7['text'] == 'X' and button8['text'] == 'X' and button9['text'] == 'X' or
         button1['text'] == 'X' and button5['text'] == 'X' and button9['text'] == 'X' or
         button3['text'] == 'X' and button5['text'] == 'X' and button7['text'] == 'X' or
-        button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
         button1['text'] == 'X' and button4['text'] == 'X' and button7['text'] == 'X' or
         button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X' or
-            button7['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X'):
-        disableButton()
-        messagebox.showinfo("Tic Tac Toe", player1)
+            button3['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X'):
+        handleButtonState(DISABLED)
+        messagebox.showinfo("Tic Tac Toe", f'{player_1_name.get()} wins')
+        player_1_score.set(player_1_score.get() + 2)
 
-    elif(flag == 8):
+    elif(flag.get() == 8):
+        handleButtonState(DISABLED)
         messagebox.showinfo("Tic Tac Toe", "Oh bummer! It's a tie")
+        player_1_score.set(player_1_score.get() + 1)
+        player_2_score.set(player_2_score.get() + 1)
 
     elif (button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
           button4['text'] == 'O' and button5['text'] == 'O' and button6['text'] == 'O' or
           button7['text'] == 'O' and button8['text'] == 'O' and button9['text'] == 'O' or
           button1['text'] == 'O' and button5['text'] == 'O' and button9['text'] == 'O' or
           button3['text'] == 'O' and button5['text'] == 'O' and button7['text'] == 'O' or
-          button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
           button1['text'] == 'O' and button4['text'] == 'O' and button7['text'] == 'O' or
-          button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O' or
-          button7['text'] == 'O' and button6['text'] == 'O' and button9['text'] == 'O'):
-        disableButton()
-        messagebox.showinfo("Tic Tac Toe", player2)
+          button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O'):
+        handleButtonState(DISABLED)
+        messagebox.showinfo("Tic Tac Toe", f'{player_2_name.get()} wins')
+        player_2_score.set(player_2_score.get() + 2)
 
 
-def btnClick(buttons):
-    global bclick, flag, name1_var, name2_var, player2, player1
+def show_score():
 
-    if buttons["text"] == " " and bclick == True:
-        buttons["text"] = "X"
+    score_player1_label = Label(root, textvariable=player_1_score, width=5, borderwidth=2,
+                                bg='white', font=('COMIC SANS MS', 15))
+    score_player1_label.grid(row=0, column=8)
 
-        bclick = False
-        player2 = name2_var.get() + " wins!"
-        player1 = name1_var.get() + " wins!"
-        checkForWin()
-        flag += 1
-
-    elif(buttons["text"] == " " and bclick == False):
-        buttons["text"] = "O"
-        bclick = True
-        checkForWin()
-        flag += 1
-    else:
-        messagebox.showinfo(
-            "Tic Tac Toe", "Button already clicked, please try again")
+    score_player2_label = Label(root, textvariable=player_2_score, width=5, borderwidth=2,
+                                bg='white', font=('COMIC SANS MS', 15))
+    score_player2_label.grid(row=1, column=8)
 
 
-def reset():
+def clear_board():
+    flag.set(0)
+    handleButtonState(NORMAL)
+    whos_turn.set(f'{player_1_name.get()} turn')
     button1['text'] = " "
     button2['text'] = " "
     button3['text'] = " "
@@ -202,6 +208,12 @@ def reset():
     button8['text'] = " "
     button9['text'] = " "
 
+
+def new_game():
+    clear_board()
+    player_1_score.set(0)
+    player_2_score.set(0)
+
 # Create the labels and the input user names
 
 
@@ -211,7 +223,8 @@ def create_widgets():
     label_player1_name = Label(root, text="Player 1 name: ",
                                font=('COMIC SANS MS', 15, 'bold'), borderwidth=0, background='white')
     label_player1_name.grid(row=0, column=6)
-    input_player1_name = Entry(root, textvariable=name1_var, width=15, borderwidth=2,
+
+    input_player1_name = Entry(root, textvariable=player_1_name, width=15, borderwidth=2,
                                bg='white', font=('COMIC SANS MS', 15))
     input_player1_name.grid(row=0, column=7)
     input_player1_name.insert(0, "Player 1")
@@ -220,46 +233,32 @@ def create_widgets():
     label_player2_name = Label(root, text="Player 2 name: ",
                                font=('COMIC SANS MS', 15, 'bold'), borderwidth=0, background='white')
     label_player2_name.grid(row=1, column=6)
-    input_player2_name = Entry(root, textvariable=name2_var, width=15, borderwidth=2,
+
+    input_player2_name = Entry(root, textvariable=player_2_name, width=15, borderwidth=2,
                                bg='white', font=('COMIC SANS MS', 15))
     input_player2_name.grid(row=1, column=7)
     input_player2_name.insert(0, "Player 2")
 
-    # # Create the "start!" button
-    # start_btn = Button(root, width=15,  text='Start', font=(
-    #     'COMIC SANS MS', 15, 'bold'), command=create_board)
-    # start_btn.grid(row=2, column=7)
-
-    # Create the "show score" button
-    show_score_btn = Button(root, width=15,  text='Show Score', font=(
-        'COMIC SANS MS', 15, 'bold'))
-    show_score_btn.grid(row=3, column=7)
-
-    # Create the "reset" button
-    # reset_btn = Button(root, width=15, text='Reset', font=(
-    #     'COMIC SANS MS', 15, 'bold'), command=reset)
-    # reset_btn.grid(row=4, column=7)
-
-   # Create the "start!" button
-    # start_btn_pic = PhotoImage(file="images/start.png")
-    # start_btn = Button(root, image=start_btn_pic, bg="white", borderwidth=0)
-    # start_btn.grid(row=4, column=7)
-
     # show_score_btn_pic = PhotoImage(file="images/show_score.png")
-    # show_score_btn = Button(
-    #     root, image=show_score_btn_pic, bg="white", borderwidth=0)
-    # show_score_btn.grid(row=4, column=7)
+    global show_score_btn_pic
+    show_score_btn_pic = PhotoImage(file="images/show_score.png")
+    show_score_btn = Button(
+        root, image=show_score_btn_pic, bg="white", borderwidth=0, command=show_score)
+    show_score_btn.grid(row=4, column=7)
 
-    # Create the "show score" button
-    # show_score_btn = Button(root, width=15,  text='Show Score', font=(
-    #     'COMIC SANS MS', 15, 'bold'))
-    # show_score_btn.grid(row=3, column=7)
+    # Create the "clear board" button
+    global clear_board_btn_pic
+    clear_board_btn_pic = PhotoImage(file="images/clear.png")
+    clear_board_btn = Button(root, image=clear_board_btn_pic,
+                             bg="white", borderwidth=0, command=clear_board)
+    clear_board_btn.grid(row=5, column=7)
 
-    # Create the "reset" button
-    reset_btn_pic = PhotoImage(file="images/reset.png")
-    reset_btn = Button(root, image=reset_btn_pic,
-                       bg="pink", borderwidth=0, command=reset)
-    reset_btn.grid(row=4, column=7)
+    # Create the "new" button
+    global new_btn_pic
+    new_btn_pic = PhotoImage(file="images/new.png")
+    new_btn = Button(root, image=new_btn_pic,
+                     bg="white", borderwidth=0, command=new_game)
+    new_btn.grid(row=3, column=7)
 
 
 root.mainloop()
